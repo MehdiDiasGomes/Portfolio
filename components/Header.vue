1976f50b8694b04801388566ad992272f28e00b9
MehdiDiasGomes <diasgomes.mehdi270@gmail.com>
2025-02-09 15:58:59 +0100

    ✨🌐 feat: add i18n

--- components/Header.vue
+++ components/Header.vue
@@ -1,14 +1,18 @@
 <template>
   <div class="h-screen flex items-center justify-center">
     <div class="max-w-[800px] flex flex-col gap-3 mx-auto text-center z-10">
-      <h1 class="text-white text-5xl">Transforming Concepts into Seamless
+      <h1 class="text-white text-5xl">{{ t('header.title') }}
         <span class="text-[#CBACF9]">
-          User Experiences
+          {{ t('header.span_title') }}
         </span>
       </h1>
       <h2 class="text-white text-1xl">
-        Hi! I’m Mehdi DIAS GOMES, a web Developer based in France
+        {{ t('header.description') }}
       </h2>
     </div>
   </div>
 </template>
+
+<script lang="ts" setup>
+const { t } = useI18n()
+</script>
