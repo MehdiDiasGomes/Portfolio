dbf85163a8be7b3ab37def8a526aeb949e0e6890
MehdiDiasGomes <diasgomes.mehdi270@gmail.com>
2025-02-09 15:37:55 +0100

    ✨ feat: create header et decoration component

---
+++ components/Decoration.vue
@@ -0,0 +1,7 @@
+<template>
+  <div>
+    <img class="absolute top-0 left-0" src="../assets/img/top_left_spotlight.png" alt="Lumière coin gauche">
+    <img class="absolute top-0 right-0" src="../assets/img/top_right_spotlight.png" alt="Lumière coin droit">
+    <img class="absolute top-0" src="../assets/img/grid_input.png" alt="Grille">
+  </div>
+</template>
---
+++ components/Header.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="h-screen flex items-center justify-center">
+    <div class="max-w-[800px] flex flex-col gap-3 mx-auto text-center z-10">
+      <h1 class="text-white text-5xl">Transforming Concepts into Seamless
+        <span class="text-[#CBACF9]">
+          User Experiences
+        </span>
+      </h1>
+      <h2 class="text-white text-1xl">
+        Hi! I’m Mehdi DIAS GOMES, a web Developer based in France
+      </h2>
+    </div>
+  </div>
+</template>
