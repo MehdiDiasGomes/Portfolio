187287757a763965d8185c606ec052a9bc51821a
MehdiDiasGomes <diasgomes.mehdi270@gmail.com>
2025-02-09 23:36:07 +0100

    ✨💄 feat(ui): add button see my work on header component

--- components/Header.vue
+++ components/Header.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="h-screen flex items-center justify-center">
-    <div class="max-w-[800px] flex flex-col gap-3 mx-auto text-center z-10">
-      <h1 class="text-white font-extrabold text-6xl">{{ t('header.title') }}
+    <div class="max-w-[800px] flex flex-col items-center gap-7 mx-auto text-center z-10">
+      <h1 class="text-white s font-extrabold leading-tight text-6xl">{{ t('header.title') }}
         <span class="text-primaryPerso">
           {{ t('header.span_title') }}
         </span>
@@ -9,6 +9,11 @@
       <h2 class="text-secondary text-xl mx-auto max-w-[500px]">
         {{ t('header.description') }}
       </h2>
+      <Button
+        class="border border-borderPerso bg-gradient-to-r from-slate-900 to-[#0d1132] rounded-xl py-7 px-8 transition duration-300 hover:scale-105">{{
+          t('button.see_work') }}
+        <Icon :stroke-width="1.75" name="ArrowUpRight" />
+      </Button>
     </div>
   </div>
 </template>
