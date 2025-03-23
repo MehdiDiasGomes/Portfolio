1474540585478b0d2b030c008fd0e754a33585c9
MehdiDiasGomes <diasgomes.mehdi270@gmail.com>
2025-03-23 16:39:19 +0100

    🚩🌐 feat(lang): add work experience title

--- components/Header.vue
+++ components/Header.vue
@@ -1,21 +1,22 @@
 <template>
   <div class="relative">
-    <Decoration class="opacity-50 absolute inset-0 h-full w-full" />
-
-    <div class="flex h-[50rem] items-center justify-center relative z-10">
+    <Decoration class="opacity-100 absolute inset-0 h-full w-full" />
+    <div class="flex h-screen items-center justify-center relative z-10">
       <div class="max-w-[800px] p-10 md:p-0 flex flex-col items-center gap-7 mx-auto text-center">
         <h1 class="text-white font-extrabold leading-tight text-4xl sm:text-6xl">
-          {{ t('header.title') }}
+          {{ t("header.title") }}
           <span class="text-primaryPerso">
-            {{ t('header.span_title') }}
+            {{ t("header.span_title") }}
           </span>
         </h1>
         <h2 class="text-secondary sm:text-xl mx-auto max-w-[500px]">
-          {{ t('header.description') }}
+          {{ t("header.description") }}
         </h2>
         <Button
-          class="border border-borderPerso bg-gradient-to-r from-slate-900 to-[#0d1132] rounded-xl py-7 px-8 transition duration-300 hover:scale-105">
-          {{ t('button.see_work') }}
+          class="border border-borderPerso bg-primary hover:bg-primaryPerso rounded-xl py-7 px-8 transition duration-300 hover:scale-105"
+          @click="scrollToBot"
+        >
+          {{ t("button.see_work") }}
           <Icon :stroke-width="1.75" name="ArrowUpRight" />
         </Button>
       </div>
@@ -23,7 +24,13 @@
   </div>
 </template>
 
-
 <script lang="ts" setup>
-const { t } = useI18n()
+const { t } = useI18n();
+
+const scrollToBot = () => {
+  window.scrollTo({
+    top: window.innerHeight,
+    behavior: "smooth",
+  });
+};
 </script>
--- components/WorkXp.vue
+++ components/WorkXp.vue
@@ -1,11 +1,17 @@
 <template>
   <div class="flex flex-col z-30 items-center justify-center">
-    <h2 class="text-white text-5xl text-center font-extrabold">My <span class="text-primaryPerso">work experience</span></h2>
+    <h2 class="text-white text-5xl text-center font-extrabold">
+      {{ t("workXp.title_first") }}
+      <span class="text-primaryPerso"> {{ t("workXp.title_second") }}</span>
+    </h2>
     <div class="max-w-[1200px] py-10">
-      <div class="grid grid-cols-1 lg:grid-cols-2 gap-8 p-6 rounded-xl ">
-        <div v-for="(item, index) in workXp" :key="index"
-          class="bg-gradient-to-r hover:scale-95 duration-200 transition-all from-[#04071D] to-[#0C0E23] shadow-[#1A1C3A] p-10 flex gap-4 items-center text-white rounded-lg shadow-md">
-          <img :src="item.icon" :alt="item.alt" class="w-24 h-20">
+      <div class="grid grid-cols-1 lg:grid-cols-2 gap-8 p-6 rounded-xl">
+        <div
+          v-for="(item, index) in workXp"
+          :key="index"
+          class="bg-gradient-to-r duration-200 transition-all from-[#04071D] to-[#0C0E23] p-10 flex gap-4 items-center text-white rounded-lg"
+        >
+          <img :src="item.icon" :alt="item.alt" class="w-24 h-20" />
           <div class="flex flex-col gap-2">
             <h3 class="text-xl font-extrabold">{{ item.title }}</h3>
             <p class="text-sm">{{ item.description }}</p>
@@ -16,39 +22,38 @@
   </div>
 </template>
 
-
 <script lang="ts" setup>
-import frontXp from '~/assets/img/frontXp.png'
-import backXp from '~/assets/img/backXp.png'
-import mobile from '~/assets/img/mobile.png'
-import amp from '~/assets/img/amp.png'
+import frontXp from "~/assets/img/frontXp.png";
+import backXp from "~/assets/img/backXp.png";
+import mobile from "~/assets/img/mobile.png";
+import amp from "~/assets/img/amp.png";
 
-const { t } = useI18n()
+const { t } = useI18n();
 
 const workXp = computed(() => [
   {
     icon: frontXp,
-    alt: 'Icon front end',
-    title: t('workXp.cardOne.title'),
-    description: t('workXp.cardOne.description'),
+    alt: "Icon front end",
+    title: t("workXp.cardOne.title"),
+    description: t("workXp.cardOne.description"),
   },
   {
     icon: backXp,
-    alt: 'Icon front end',
-    title: t('workXp.cardTwo.title'),
-    description: t('workXp.cardTwo.description'),
+    alt: "Icon front end",
+    title: t("workXp.cardTwo.title"),
+    description: t("workXp.cardTwo.description"),
   },
   {
     icon: mobile,
-    alt: 'Icon front end',
-    title: t('workXp.cardThree.title'),
-    description: t('workXp.cardThree.description'),
+    alt: "Icon front end",
+    title: t("workXp.cardThree.title"),
+    description: t("workXp.cardThree.description"),
   },
   {
     icon: amp,
-    alt: 'Icon front end',
-    title: t('workXp.cardFour.title'),
-    description: t('workXp.cardFour.description'),
-  }
-]) 
+    alt: "Icon front end",
+    title: t("workXp.cardFour.title"),
+    description: t("workXp.cardFour.description"),
+  },
+]);
 </script>
