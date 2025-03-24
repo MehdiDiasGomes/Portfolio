d3434e0d919c78dff13f77a13e7ca45543866f6f
MehdiDiasGomes <diasgomes.mehdi270@gmail.com>
2025-03-24 08:40:31 +0100

    ✨ feat: add button on header to scroll down when click

--- components/Header.vue
+++ components/Header.vue
@@ -2,7 +2,7 @@
   <div class="relative">
     <Decoration class="opacity-100 absolute inset-0 h-full w-full" />
     <div class="flex h-screen items-center justify-center relative z-10">
-      <div class="max-w-[800px] p-10 md:p-0 flex flex-col items-center gap-7 mx-auto text-center">
+      <div class="max-w-[800px] p-10 md:p-0 flex flex-col items-center gap-10 mx-auto text-center">
         <h1 class="text-white font-extrabold leading-tight text-4xl sm:text-6xl">
           {{ t("header.title") }}
           <span class="text-primaryPerso">
@@ -13,11 +13,10 @@
           {{ t("header.description") }}
         </h2>
         <Button
-          class="border border-borderPerso bg-primary hover:bg-primaryPerso rounded-xl py-7 px-8 transition duration-300 hover:scale-105"
+          class="border cursor-pointer flex items-center justify-center border-borderPerso hover:bg-gray-700 rounded-xl p-4 transition duration-300 hover:scale-105"
           @click="scrollToBot"
         >
-          {{ t("button.see_work") }}
-          <Icon :stroke-width="1.75" name="ArrowUpRight" />
+          <Icon h="1.75" name="ArrowBigDownDash" />
         </Button>
       </div>
     </div>
