23768e481ae8ab41d423e2dbae6faf349d46dfdf
MehdiDiasGomes <diasgomes.mehdi270@gmail.com>
2025-03-30 15:58:04 +0200

    ✨ feat: add captcha plugin to nuxt.config.ts

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,6 +2,9 @@
 export default defineNuxtConfig({
   compatibilityDate: "2024-11-01",
   devtools: { enabled: true },
+  plugins:[
+          {src: '~/plugins/vue-recaptcha-v3.ts', mode: 'client' },
+  ],
   modules: [
     "@nuxtjs/tailwindcss",
     "@nuxtjs/i18n",
