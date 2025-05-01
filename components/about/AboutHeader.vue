59773e7ef203d3101ecbdd01755b2576a2d803da
MehdiDiasGomes <diasgomes.mehdi270@gmail.com>
2025-05-01 19:52:47 +0200

    ✨ feat(about): add AboutHeader component with avatar image

---
+++ components/about/AboutHeader.vue
@@ -0,0 +1,8 @@
+<template>
+    <div class="flex">
+        <img src="/assets/img/Avatar.jpg" alt="Photo de profil" class="rounded-full w-32 h-32 mr-4" />
+    </div>
+</template>
+
+<script setup lang="ts">
+</script>
