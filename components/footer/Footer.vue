c9cd4f4f583a8751762b2cdd831fa49bafe722ff
MehdiDiasGomes <diasgomes.mehdi270@gmail.com>
2025-05-04 18:26:40 +0200

    🐛💄 fix(ui): adjust margin-top for footer component

--- components/footer/Footer.vue
+++ components/footer/Footer.vue
@@ -1,5 +1,5 @@
 <template>
-  <footer class="bg-background/5 bottom-0 w-full text-white py-8 mt-auto">
+  <footer class="bg-background/5 bottom-0 w-full text-white py-8 mt-8">
     <div class="container mx-auto px-4">
       <div class="flex flex-col md:flex-row justify-between items-center">
         <div class="mb-4 md:mb-0">
