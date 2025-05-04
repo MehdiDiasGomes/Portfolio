ea63516fba8f9a48798fa6a38a03506a4a234126
MehdiDiasGomes <diasgomes.mehdi270@gmail.com>
2025-05-04 19:39:50 +0200

    ✨💼 feat: enhance About section with experience details and add WorkExperience icon

--- components/about/About.vue
+++ components/about/About.vue
@@ -1,13 +1,12 @@
 <template>
-  <div class="relative mp-home mt-40 flex justify-center items-center">
-    <div class="flex flex-col">
+  <div class="relative mp-home mt-40 xl:mt-64 flex justify-center items-center">
+    <div class="flex flex-col gap-20">
       <AboutHeader />
+      <AboutExperience />
     </div>
   </div>
 </template>
 
 <script lang="ts" setup>
-import { personalFacts, socials } from "~/constants/about";
-
 const { t } = useI18n();
 </script>
