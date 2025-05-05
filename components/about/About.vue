99d69589d282a22ba07ba382dbf5f473b48590c9
MehdiDiasGomes <diasgomes.mehdi270@gmail.com>
2025-05-05 08:38:47 +0200

    ✨✏️ feat: fix component name typo and add AboutSkills component to About section

--- components/about/About.vue
+++ components/about/About.vue
@@ -2,11 +2,14 @@
   <div class="relative mp-home mt-40 xl:mt-64 flex justify-center items-center">
     <div class="flex flex-col gap-20">
       <AboutHeader />
-      <AboutExperience />
+      <AboutExperiences />
+      <AboutSkills />
     </div>
   </div>
 </template>
 
 <script lang="ts" setup>
+import AboutExperiences from './AboutExperiences.vue';
+
 const { t } = useI18n();
 </script>
